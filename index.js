3fc10f9a4048d3ac02bedf32283973983d3f035b
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-08-20 12:56:35 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1=2;
+let num2=31;
+let multiply=num1*num2;
 
-// Write your code here
+//random integer
+let random =Math.floor(Math.random()*10)
+console.log(random)
+
+//remainder
+let num3=24;
+let num4=5;
+let mod=num3%num4
+
+//max integer
+let max=Math.max(6,4,6,8,20,6,1,0)
